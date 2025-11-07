bef95382cd5fb0806e8113f90c927ff9ec51b4fa
DoktorHeidi <kaulfuerst.soboll@gmail.com>
2025-11-07 21:40:02 +0100

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def app():
     st.title("Klicke bitte weiter ...\n")
-    st.write("Lindas Teil ist aus Gründen des Urheberrechts nicht implementiert.")
+    st.write("Hier ist das neue Projekt leider noch nicht implementiert.")
     st.write("Bitte gehe zu '📊 Relevanz-Check von Allergien'.")
