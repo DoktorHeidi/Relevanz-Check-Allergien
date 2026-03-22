6a9c736da834ebda21cb43ff61b5a3a14ed0bbe2
DoktorHeidi <kaulfuerst.soboll@gmail.com>
2026-03-22 15:43:26 +0100

    Update timeline.py
    Wort geändert

--- timeline.py
+++ timeline.py
@@ -226,7 +226,7 @@ def app():
             "- Analyse 1 wurde zur Übersicht der Daten durchgeführt.\n"
             "- Analyse 2 wurde mit standardisierten Daten durchgeführt, um mit kMeans Muster erkennen zu können.\n"
             
-            "- Analyse 3 wurde mit gefilterten Daten gearbeitet, wobei hier auch die Transponierung mancher Spalten nötig war.\n"
+            "- Analyse 3 wurde mit gefilterten Daten durchgeführt, wobei hier auch die Transponierung mancher Spalten nötig war.\n"
             "Ziel der dritten Analyse war es herauszufinden, ob die Anzahl der Fälle bestimmter Diagnose-Schlüssel mit Assoziation zu Allergien \nmit anderen mit Assoziation zu Herz-Kreislauf-, Rheuma- und Atemwegserkrankungen korrelieren. Zudem wurde Multiple Sklerose als Autoimmunerkrankung in die Auswahl mit aufgenommen, die sich grundsätzlich von Allergien unterscheiden.\n"
             " Zum Schluss besteht die Möglichkeit der freien Auswahl von Diagnose-Schlüsseln zur Korrelation."
             ,
